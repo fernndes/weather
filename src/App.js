2cdeebb2fac22342c6c0093ab0111b2f2006cd7b
fernndes <ferndes.gabriel@hotmail.com>
2021-05-11 17:52:53 -0300

    title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import Main from './pages/Main'
 
 function App() {
+  useEffect(() => {
+    document.title = 'Weather'
+  }, [])
   return (
     <div className="App">
       <Main />
